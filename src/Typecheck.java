be3c326233ae0b633d496cdea446529c3da2cd36
tyler-schultz <tysch@rams.colostate.edu>
2020-03-03 15:49:58 -0700

    Added all source files

---
+++ src/Typecheck.java
@@ -0,0 +1,15 @@
+import syntaxtree.Goal;
+import visitor.PrettyPrinterVisitor;
+
+public class Typecheck {
+
+  public static void main(String args[]) throws ParseException {
+
+    MiniJavaParser parser = new MiniJavaParser(System.in);
+    Goal tree = parser.Goal();
+    PrettyPrinterVisitor printer = new PrettyPrinterVisitor();
+    printer.visit(tree);
+
+  }
+
+}
